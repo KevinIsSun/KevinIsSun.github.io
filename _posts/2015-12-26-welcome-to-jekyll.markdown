a415912298f9cc84402b7ed4c1e0dc70e19f0a9b
kevin <kevinissun@163.com>
2017-04-27 11:03:51 +0800

    ..==

---
+++ _posts/2015-12-26-welcome-to-jekyll.markdown
@@ -0,0 +1,7 @@
+---
+layout: post
+title:  "年终总结"
+date:   2015-12-26 23:50:03 +0800
+categories: jekyll update
+---
+今年真是非常艰难的一年。
---
+++ xxxxx/console.js
@@ -0,0 +1,6 @@
+if (window.console) {
+    var cons = console;
+    if (cons) {
+        cons.log('曾今，现在，将来，如果遇到生命中对的那个人，请一定要好好把握呢！');
+    }
+}
